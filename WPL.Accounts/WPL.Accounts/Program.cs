bacdd00cb6b18aa3e9da38b2eadd04e22d9425a7
Adrian Zawadzki <asiqq23@hotmail.com>
2018-01-23 00:28:07 +0100

    docker build fix

--- WPL.Accounts/WPL.Accounts/Program.cs
+++ WPL.Accounts/WPL.Accounts/Program.cs
@@ -19,6 +19,7 @@ namespace WPL.Accounts
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:2222")
                 .UseStartup<Startup>()
                 .Build();
     }
